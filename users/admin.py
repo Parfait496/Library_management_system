d5799a141fdaf1f3bfdcc640bb9b93c39b03d862
Parfait <parfaitndizihiwe@gmail.com>
2026-05-08 11:37:32 +0200

    add customer User model with role-based access control

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'role', 'is_verified', 'is_active')
+    list_filter = ('role', 'is_verified', 'is_active')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Library Info', {
+            'fields': ('role', 'phone_number', 'address', 'date_of_birth',
+                       'profile_picture', 'is_verified')
+        }),
+
+    )
+
+    search_fields = ('username', 'email')
--- users/models.py
+++ users/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+     #add  roles based access
+
+    class Role(models.TextChoices):
+          ADMIN = 'ADMIN', 'admin'
+          LIBRARIAN = 'LIBRARIAN', 'librarian'
+          MEMBER = 'MEMBER', 'member'
+
+    role = models.CharField(
+         max_length=20,
+         choices=Role.choices,
+         default=Role.MEMBER,
+    )
+
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    profile_picture = models.ImageField(
+         upload_to='profile_picture/',
+         blank=True,
+         null=True
+    )
+
+    is_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+         return f"{self.username} ({self.role})"
+    
+
+    # Role helper properties (used in views and templates)
+
+    @property
+    def is_admin(self):
+         return self.role == self.Role.ADMIN
+    
+    @property
+    def is_librarian(self):
+         return self.role == self.Role.LIBRARIAN
+    
+    @property
+    def is_member(self):
+         return self.role == self.Role.MEMBER
+
+
+
+
