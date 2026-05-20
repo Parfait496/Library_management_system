2d18a551c77a2953648ba75d291868a02c645200
Parfait <parfaitndizihiwe@gmail.com>
2026-05-20 21:01:42 +0200

    allowing all hosts

--- core/settings.py
+++ core/settings.py
@@ -13,11 +13,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 DEBUG=True
-ALLOWED_HOSTS = config(
-    'ALLOWED_HOSTS',
-    default='localhost'
-).split(',')
+# ALLOWED_HOSTS = config(
+#     'ALLOWED_HOSTS',
+#     default='localhost'
+# ).split(',')
 
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
