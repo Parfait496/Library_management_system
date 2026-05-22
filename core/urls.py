076fc117400e432a75b5a4334f45c9a8d5f49dbf
Parfait <parfaitndizihiwe@gmail.com>
2026-05-22 12:32:31 +0200

    fix: add homepage redirect to login

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,14 @@
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Redirect homepage to login page
+    path('', lambda request: redirect('login'), name='home'),
 
     path('', include('users.urls')),
     path('', include('books.urls')),
