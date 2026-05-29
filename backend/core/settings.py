88bbad2267e6ed1a37d84207cd5d109bedbe4c66
Parfait <parfaitndizihiwe@gmail.com>
2026-05-29 20:37:53 +0200

    fix: clean railway deployment configuration

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -187,8 +187,8 @@ USE_TZ        = True
 # ===========================================================================
 # STATIC AND MEDIA FILES
 # ===========================================================================
-STATIC_URL  = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL   = '/media/'
 MEDIA_ROOT  = BASE_DIR / 'media'
 
