94119bc62301dad5c10c56bedbda5af0d3bbe371
Parfait <parfaitndizihiwe@gmail.com>
2026-05-31 11:26:29 +0200

    feat: simplified landing page, back button on auth pages, production emails

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -170,7 +170,10 @@ CORS_ALLOW_METHODS = [
 # ===========================================================================
 # EMAIL
 # ===========================================================================
-EMAIL_BACKEND       = config('EMAIL_BACKEND',       default='django.core.mail.backends.console.EmailBackend')
+EMAIL_BACKEND = config(
+    'EMAIL_BACKEND',
+    default='django.core.mail.backends.smtp.EmailBackend'
+)
 EMAIL_HOST          = config('EMAIL_HOST',          default='smtp.gmail.com')
 EMAIL_PORT          = config('EMAIL_PORT',          default=587, cast=int)
 EMAIL_USE_TLS       = config('EMAIL_USE_TLS',       default=True, cast=bool)
